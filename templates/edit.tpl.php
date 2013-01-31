21584ad83eb87a5d677b3fa457dd97348babf625
chris jeffries <chris+git@floatingworks.co.uk>
2013-01-31 00:25:46 +0000

    edit form template added

---
+++ templates/edit.tpl.php
@@ -0,0 +1,8 @@
+<form action=<?php echo $_SERVER['PHP_SELF']; ?> method='post'>
+	Title
+	<input type="text" name= "title" id="title" value="<?php !isset($this->var['title']) ? print '' : print $this->var['title'];?>"><br />
+	Content<br />
+	<textarea rows="20" cols="50" name="blogcontent" id="blogcontent"><?php !isset($this->var['blogcontent']) ? print '' : print $this->var['blogcontent'];?></textarea><br />
+	<input type="hidden" name="id" value="<?php print $this->var['id'];?>">
+	<input type="submit" name="submit" value="submit">
+</form>
