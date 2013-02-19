216240bdc7883c1a3580d3ad0ed08739c90cf1b7
wellbourne <chris@floatingworks.co.uk>
2013-02-19 10:10:39 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-// this file should be outside the project root with strict permissions
+// this file should be outside the project root with appropriate permissions www-data/nobody or whatever web server user should be for your system
 define('TEMPLATE_ROOT', 'path/to/template/root');
 define('DB_HOST', 'hostdb');
 define('DB_USERNAME', 'username');
