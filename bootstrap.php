640097ba15d2e563c7d0a113b46b6b04e509dce1
chris jeffries <chris+git@floatingworks.co.uk>
2013-02-22 23:44:17 +0000

    autoloader added to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,10 @@
 <?php
 
-require_once('./includes/Model.class.php');
-require_once('./includes/User.class.php');
-require_once('./includes/Controller.class.php');
-require_once('./includes/Templater.class.php');
-require_once('./includes/Model.class.php');
-require_once('./includes/BlogEntry.class.php');
-require_once('./includes/Database.class.php');
+// autoloader
+function __autoload($classname) {
+	$filename = "./includes/" . $classname . ".class.php";
+	include_once($filename);
+}
 
 // config file
 require_once('/home/blogist/config.php'); // this config is outside the project directory owned by root, with read only permissions
