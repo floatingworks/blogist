6d62908901b5b386848b9ed915b3f42b24d68cde
chris jeffries <chris+git@floatingworks.co.uk>
2013-02-23 23:19:23 +0000

    autoloader now using spl_autoload_register

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,12 @@
 <?php
 
 // autoloader
-function __autoload($classname) {
+function autoloadClasses($classname) {
 	$filename = "./includes/" . $classname . ".class.php";
 	include_once($filename);
 }
+
+spl_autoload_register('autoloadClasses');
 
 // config file
 require_once('/home/blogist/config.php'); // this config is outside the project directory owned by root, with read only permissions
--- templates/list.tpl.php
+++ templates/list.tpl.php
@@ -1,10 +1,10 @@
 <div id="results">
 <?php
-//print_r($this->var['array']);
+// print_r($this->var['array']);
 if (!empty($this->var)) {
 	foreach ($this->var['array'] as $blogentry) {
 		$timestamp = date(DATE_RFC822, $blogentry['timeposted']);
-		echo "<li><a href=\"?mode=edit&id={$blogentry['id']}\">{$blogentry['title']}</a> posted - {$timestamp}</li>";
+		echo "<li><a href=\"?mode=edit&id={$blogentry['id']}\">{$blogentry['title']}</a> {$blogentry['content']} posted - {$timestamp}</li>";
 	}
 }
 ?>
