5caad3e877ac0200c113da9eef602453f600fede
martin wellbourne <chris@floatingworks.co.uk>
2013-10-01 17:39:57 +0100

    login link added to header template

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -4,7 +4,7 @@ if ((isset($_SESSION['id']) && !empty($_SESSION['id']))) {
 	echo "You are logged in as " . $_SESSION['id'];
 	echo " <a href=\"?mode=logout\">log out</a>";
 } else {
-	echo "Please log in or <a href=\"?mode=register\">register</a>";
+	echo "Please <a href=\"?mode=login\">log in</a> or <a href=\"?mode=register\">register</a>";
 }
 
 ?>
